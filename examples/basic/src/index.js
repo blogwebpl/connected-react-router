7e1ca16bb8de9dbccd647b7e4a2a0b6eca7bebcd
mzk-bydgoszcz <tduralek@mzk.bydgoszcz.pl>
2022-11-10 08:54:13 +0100

    update to react 18

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,19 +1,21 @@
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import App from './App'
 import configureStore, { history } from './configureStore'
 
 const store = configureStore()
 const render = () => {
-  ReactDOM.render(
+  const container = document.getElementById('react-root');
+  const root = createRoot(container);
+
+  root.render(
     <AppContainer>
       <Provider store={store}>
         <App history={history} />
       </Provider>
-    </AppContainer>,
-    document.getElementById('react-root')
+    </AppContainer>
   )
 }
 
--- examples/immutable/src/index.js
+++ examples/immutable/src/index.js
@@ -2,20 +2,22 @@ import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import Immutable from 'immutable'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+
 import App from './App'
 import configureStore, { history } from './configureStore'
 
 const initialState = Immutable.Map()
 const store = configureStore(initialState)
+const container = document.getElementById('react-root');
+const root = createRoot(container);
 const render = () => {
-  ReactDOM.render(
+  root.render(
     <AppContainer>
       <Provider store={store}>
         <App history={history} />
       </Provider>
-    </AppContainer>,
-    document.getElementById('react-root')
+    </AppContainer>
   )
 }
 
